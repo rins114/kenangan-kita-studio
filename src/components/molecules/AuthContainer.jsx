4eaa250519d7588651c01994139e2c6ccc44ad8e
diazkhalid <diazananda02@gmail.com>
2025-01-27 20:57:09 +0800

    minor fix

--- src/components/molecules/AuthContainer.jsx
+++ src/components/molecules/AuthContainer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function AuthContainer({ children, maxWidth = "max-w-md" }) {
+export default function AuthContainer({ children, maxWidth = "28rem" }) {
   return (
     <div
-      className={`z-[80] flex flex-col justify-center items-center p-7 border-2 rounded-sm w-full sm:${maxWidth} gap-3 bg-white shadow-md`}
+      className={`z-[80] flex flex-col justify-center items-center p-7 border-2 rounded-sm w-full sm:max-w-md gap-3 bg-white shadow-md`}
+      style={{ maxWidth: `${maxWidth}` }}
     >
       {children}
     </div>
